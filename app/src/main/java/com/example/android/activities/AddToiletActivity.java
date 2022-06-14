c0c6a56af4e450ebd15b7c0f450f4c81c05cd18e
Ricky Hu <ricky9667@gmail.com>
2022-06-14 14:47:33 +0800

    Feature: make AddToiletActivity can set tissue amount

--- app/src/main/java/com/example/android/activities/AddToiletActivity.java
+++ app/src/main/java/com/example/android/activities/AddToiletActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -16,6 +18,8 @@ public class AddToiletActivity extends AppCompatActivity {
     private final Store store = Store.getInstance();
     private EditText toiletIdEditText;
     private EditText toiletLocationEditText;
+    private TextView tissueAmountTextView;
+    private SeekBar tissueAmountSeekBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,14 +27,31 @@ public class AddToiletActivity extends AppCompatActivity {
         setContentView(R.layout.add_toilet_activity);
         toiletIdEditText = findViewById(R.id.toiletIdEditText);
         toiletLocationEditText = findViewById(R.id.toiletLocationEditText);
+        tissueAmountTextView = findViewById(R.id.tissueAmountTextView);
+        tissueAmountSeekBar = findViewById(R.id.tissueAmountSeekBar);
+        tissueAmountSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                tissueAmountTextView.setText("Tissue amount: " + i + "%");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {}
+        });
     }
 
     public void submitNewToilet(View view) {
         Intent replyIntent = new Intent();
         final String toiletId = toiletIdEditText.getText().toString();
         final String toiletLocation = toiletLocationEditText.getText().toString();
+        final double percentage = tissueAmountSeekBar.getProgress() / 100.0f;
+        ToiletState state = percentage >= 0.1f ? ToiletState.SUFFICIENT : ToiletState.INSUFFICIENT;
+
         int restroomIndex = store.getShowingRestroomIndex();
-        store.addToilet(restroomIndex, new Toilet(toiletId, toiletLocation, ToiletState.DISCONNECTED, -1.0f));
+        store.addToilet(restroomIndex, new Toilet(toiletId, toiletLocation, state, percentage));
         setResult(RESULT_OK, replyIntent);
         finish();
     }
