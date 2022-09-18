f0f7c51f220902b1226c9e71699b8bf04f434c8c
Ricky Hu <ricky9667@gmail.com>
2022-09-18 12:25:51 +0800

    fix: save toilet to restroom according to restroomId from ToiletActivity

--- app/src/main/java/com/example/android/view/ToiletActivity.java
+++ app/src/main/java/com/example/android/view/ToiletActivity.java
@@ -15,30 +15,15 @@ import com.example.android.viewModel.ToiletsViewModel;
 
 public class ToiletActivity extends AppCompatActivity {
     private ToiletsViewModel _viewModel;
-    private ToiletListAdapter _adapter;
+    private String _restroomId;
+    private String _restroomLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.toilet_activity);
-        String restroomId = getIntent().getStringExtra("restroomId");
-        String restroomLocation = getIntent().getStringExtra("restroomLocation");
 
-        if (restroomId != null) {
-            _adapter = new ToiletListAdapter(this);
-            _viewModel = new ToiletsViewModel(_adapter);
-
-            RecyclerView toiletRecyclerView = findViewById(R.id.toiletRecyclerView);
-            toiletRecyclerView.setAdapter(_adapter);
-            toiletRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-            final Runnable backgroundTask = () -> _viewModel.loadToiletsData(restroomId);
-            new BasicAsyncTask(backgroundTask, _adapter::notifyDataSetChanged).execute();
-        }
-
-        if (restroomId != null) {
-            setTitle(restroomLocation);
-        }
+        initActivityInformation();
     }
 
     @Override
@@ -46,13 +31,37 @@ public class ToiletActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == 1) {
             if (resultCode == RESULT_OK) {
-                _adapter.notifyDataSetChanged();
+                initActivityInformation();
             }
+        }
+    }
+
+    private void initActivityInformation() {
+        Intent intent = getIntent();
+        _restroomId = intent.getStringExtra("restroomId");
+        _restroomLocation = intent.getStringExtra("restroomLocation");
+
+        if (_restroomId != null) {
+            ToiletListAdapter _adapter = new ToiletListAdapter(this);
+            _viewModel = new ToiletsViewModel(_adapter);
+
+            RecyclerView toiletRecyclerView = findViewById(R.id.toiletRecyclerView);
+            toiletRecyclerView.setAdapter(_adapter);
+            toiletRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+            final Runnable backgroundTask = () -> _viewModel.loadToiletsData(_restroomId);
+            new BasicAsyncTask(backgroundTask, _adapter::notifyDataSetChanged).execute();
+        }
+
+        if (_restroomLocation != null) {
+            setTitle(_restroomLocation);
         }
     }
 
     public void addNewToilet(View view) {
         Intent intent = new Intent(view.getContext(), AddToiletActivity.class);
+        intent.putExtra("restroomId", _restroomId);
+        intent.putExtra("restroomLocation", _restroomLocation);
         startActivityForResult(intent, 1);
     }
 }
